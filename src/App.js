05bbc8db632634d6f9f5c149c6b5b5eab950ff01
Helena Oliveira <helena.oliveira@casaldev.com.br>
2020-08-01 11:09:36 -0300

    transfer github repository

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react'
+import api from './services/api'
+
+import './styles.css'
+
+function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
+  async function handleAddRepository() {
+    const response = await api.post('repositories', {
+      url: 'https://github.com/josepholiveira',
+      title: `Novo repository ${Date.now()}`,
+      techs: ['React', 'Node']
+    })
+
+    const repository = response.data
+
+    setRepositories([...repositories, repository])
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`)
+
+    const newRepositories = repositories.filter(repository => {
+      return repository.id !== id
+    })
+
+    setRepositories([...newRepositories])
+  }
+
+  return (
+    <div>
+      <ul data-testid='repository-list'>
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+
+            <button onClick={() => handleRemoveRepository(repository.id)}>Remover</button>
+          </li>
+        ))}
+      </ul>
+
+      <button type='button' onClick={handleAddRepository}>
+        Adicionar
+      </button>
+    </div>
+  )
+}
+
+export default App
